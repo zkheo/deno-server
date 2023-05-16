6a04701ca4ba887627410ef7dca121507efbb347
zhangk31 <zhangk31@ziroom.com>
2023-05-16 19:57:05 +0800

    1

--- config/mysqlConfig.ts
+++ config/mysqlConfig.ts
@@ -1,21 +1,10 @@
-// import { Client } from "https://deno.land/x/mysql/mod.ts";
-//
-// const client = await new Client().connect({
-//     hostname: "aws.connect.psdb.cloud",
-//     username: "yn46of4gql8pfene5occ",
-//     db: "mybase",
-//     password: "pscale_pw_B10ZUYdFRwM3xZZWRTUayVZYF3JvuJZKFk53eWB9A5s",
-// });
-//
-//
-// export default client;
+import { Client } from "https://deno.land/x/mysql/mod.ts";
 
-import { connect } from "npm:@planetscale/database@^1.2";
-const config = {
-    host: 'aws.connect.psdb.cloud',
-    username: 'yn46of4gql8pfene5occ',
-    password: 'pscale_pw_B10ZUYdFRwM3xZZWRTUayVZYF3JvuJZKFk53eWB9A5s',
-};
-const client = connect(config);
+const client = await new Client().connect({
+    hostname: "mysql.sqlpub.com",
+    username: "wolikezk",
+    db: "zkbase",
+    password: "f25f6b04b8c49c8b",
+});
 
 export default client;
